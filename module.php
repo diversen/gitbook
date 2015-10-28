427720c0b6783db13b4f3b308e161898b0c44fca
Dennis Iversen <dennis.iversen@gmail.com>
2015-10-28 11:42:40 +0100

    best option .then

--- module.php
+++ module.php
@@ -650,22 +650,8 @@ class module {
                 });
             }
             
-            //checkoutFiles().pipe(createEpub().pipe(createHtml().pipe(createMobi().pipe(createChunked().pipe(createPdf)))));
-            // Non-blocking
-            checkoutFiles().pipe(createEpub().pipe(createHtml().pipe(createMobi().pipe(createChunked().pipe(createPdf)))));
-            // $.when(checkoutFiles()).then(createEpub()).then(createHtml(), createMobi(), createChunked(), createPdf());
-            
-            /*
-            var dfd = $.Deferred();
-            dfd.done(checkoutFiles)
-                    .done(createHtml)
-                    .done(createChunked)
-                    .done(createEpub)
-                    .done(createMobi)
-                    .done(createPdf);
-            
-            dfd.resolve();
-            */
+            checkoutFiles().then(createEpub().then(createHtml().then(createMobi().then(createChunked().then(createPdf)))));
+
         </script>
        
         
